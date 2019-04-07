ffabf7b2599ec5bae41d696c70498b7823a199b2
Owen Powell <opowell2899@gmail.com>
2019-04-06 20:00:34 -0400

    Got input set up along with uppercase function

--- main.cpp
+++ main.cpp
@@ -1,10 +1,40 @@
 #include <iostream>
+#include <vector>
 #include <boost/random/random_device.hpp>
 #include <boost/random/uniform_int_distribution.hpp>
 
+//function to change input to all upper case
+std::string inputToUpper(std::string lowercaseInput) {
+
+    std::transform(lowercaseInput.begin(), lowercaseInput.end(), lowercaseInput.begin(), ::toupper);
+
+    return lowercaseInput;
+}
+
 int main() {
 
+    std::cout << "-----------------------------------------" << std::endl;
     std::cout << "----Welcome to the password generator----" << std::endl;
+    std::cout << "------Please select an option below------" << std::endl;
+    std::cout << "-----------------------------------------" << std::endl;
+
+    std::cout << " Generate " << std::endl;
+    std::cout << " Store " << std::endl;
+
+    std::string userInput;
+
+    getline(std::cin, userInput);
+
+    //change userInput to all uppercase for reduced possibility of missing an input option
+    userInput = inputToUpper(userInput);
+
+    if (userInput == "GENERATE") {
+        //ask for password length and generate here
+    }
+
+    if (userInput == "STORE") {
+        //save passwords in vector here
+    }
 
     return 0;
 }
