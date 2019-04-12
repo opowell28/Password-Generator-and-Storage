affba997ede759d5c29cb487f2825643622875ac
Owen Powell <opowell2899@gmail.com>
2019-04-12 15:57:39 -0400

    Password generator fully functions as well as password storage and retrieval.

--- main.cpp
+++ main.cpp
@@ -11,44 +11,96 @@ std::string inputToUpper(std::string lowercaseInput) {
     return lowercaseInput;
 }
 
-int main() {
-
+//function which displays the main menu and takes input
+void mainMenu(std::string &menuSelection) {
     std::cout << "-----------------------------------------" << std::endl;
     std::cout << "----Welcome to the password generator----" << std::endl;
     std::cout << "------Please select an option below------" << std::endl;
     std::cout << "-----------------------------------------" << std::endl;
 
-    std::cout << " Generate " << std::endl;
-    std::cout << " Store " << std::endl;
+    std::cout << "1. Generate " << std::endl;
+    std::cout << "2. Store " << std::endl;
+    std::cout << "3. Retrieve " << std::endl;
+    std::cout << "4. Exit " << std::endl;
 
-    std::string userInput;
+    //take input for menu selections
+    std::cin >> menuSelection;
+}
 
-    getline(std::cin, userInput);
+int main() {
 
-    //change userInput to all uppercase for reduced possibility of missing an input option
-    userInput = inputToUpper(userInput);
+    //variable to take user menu selection
+    std::string menuInput;
 
-    if (userInput == "GENERATE") {
-        int passLength = 0;
-        std::cout << "How long do you want your password to be? " << std::endl;
-        std::cin >> passLength;
+    //display the main menu
+    mainMenu(menuInput);
 
-        std::string chars(
-                "abcdefghijklmnopqrstuvwxyz"
-                "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                "!@#$%^&*"
-                "`-_,./'|[]{}");
-        boost::random::random_device rng;
-        boost::random::uniform_int_distribution<> index_dist(0, chars.size() - 1);
-        for (int i = 0; i < passLength; ++i) {
-            std::cout << chars[index_dist(rng)];
+    //vector to store passwords
+    std::vector<std::string> passwordVec;
+
+    //change userInput to all uppercase to reduce possibility of missing an input option
+    menuInput = inputToUpper(menuInput);
+
+    while (menuInput != "EXIT" || menuInput != "4" || menuInput != "4.") {
+
+        //generate a random password
+        if (menuInput == "GENERATE" || menuInput == "1" || menuInput == "1.") {
+            //variable to store desired password length
+            int passLength = 0;
+            std::cout << "How long do you want your password to be? " << std::endl;
+            std::cin >> passLength;
+
+            //all chars that can be selected for a password
+            std::string chars(
+                    "abcdefghijklmnopqrstuvwxyz"
+                    "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                    "!@#$%^&*"
+                    "`-_,./'|[]{}");
+
+            //create device to get random characters
+            boost::random::random_device rng;
+            boost::random::uniform_int_distribution<> index_dist(0, chars.size() - 1);
+
+            std::cout << "Here is your password: " << std::endl;
+            //output the randomized characters
+            for (int i = 0; i < passLength; ++i) {
+                std::cout << chars[index_dist(rng)];
+            }
+            std::cout << std::endl;
+            //return to the main menu
+            mainMenu(menuInput);
         }
-        std::cout << std::endl;
-    }
 
-    if (userInput == "STORE") {
-        //save passwords in vector here
-    }
+            //allow user to store passwords
+        else if (menuInput == "STORE" || menuInput == "2" || menuInput == "2.") {
+            std::string savePass;
+            std::cout << "Enter the password you would like to store: " << std::endl;
+            std::cin >> savePass;
 
-    return 0;
+            //store saved passwords in a vector
+            passwordVec.push_back(savePass);
+            mainMenu(menuInput);
+        }
+
+            //retrieve all stored passwords
+        else if (menuInput == "RETRIEVE" || menuInput == "3" || menuInput == "3.") {
+            int i = 0;
+            std::cout << "Here are all your current stored passwords: " << std::endl;
+            //output each password currently stored in the vector
+            for (std::string s : passwordVec) {
+                i++;
+                std::cout << i << ". " << s << std::endl;
+            }
+            mainMenu(menuInput);
+
+        }
+
+        //exit the program here
+        else if (menuInput == "EXIT" || menuInput == "4" || menuInput == "4.") {
+            return 0;
+        } else {
+            std::cout << "Please enter a valid menu option. " << std::endl;
+            mainMenu(menuInput);
+        }
+    }
 }
